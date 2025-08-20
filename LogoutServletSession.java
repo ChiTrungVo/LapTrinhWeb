201a2091f2339ccfbb73804b0ead14a5320fd8bf
ChiTrungVo <vochitrung212@gmail.com>
2025-08-20 15:46:17 +0700

    Create LogoutServletSession.java

---
+++ LogoutServletSession.java
@@ -0,0 +1,20 @@
+package baitap1.controllers;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate(); // hủy session
+        }
+        response.sendRedirect("login.html");
+    }
+}
